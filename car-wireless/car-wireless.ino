4449c0e9d0cc5c26c88167f9af3656221190221b
akenuLegend <nhatvuok@gmail.com>
2024-11-19 18:00:55 +0700

    add turn left, turn right

--- car-wireless/car-wireless.ino
+++ car-wireless/car-wireless.ino
@@ -1,4 +1,7 @@
 
+#define BLYNK_TEMPLATE_ID "TMPL6TFggeSyC"
+#define BLYNK_TEMPLATE_NAME "FIRST TEMPLATE"
+#define BLYNK_AUTH_TOKEN "pJ3mTZOZmNNNZuS8WPCfQqc1mQkzylAz"
 
 int EnA = 16;
 int EnB = 14;
@@ -19,22 +22,21 @@ void setup() {
 
 }
 
+void forward();
+void backward();
+void turnLeft();
+void turnRight();
+
 void loop()                                                                                                                                                   {
   // put your main code here, to run repeatedly:
 
-  forward(100,100);
-  delay(5000);
-  forward(10,10);
-  delay(1000);
-  backward(100,100);
-  delay(5000);
-  
+
 }
 
 void forward (int pwmEnA, int pwmEnB) {
 
-  analogWrite(EnB, pwmEnA);
-  analogWrite(EnA, pwmEnB);
+  analogWrite(EnB, pwmEnB);
+  analogWrite(EnA, pwmEnA);
   
   digitalWrite(ln3, LOW);
   digitalWrite(ln4, HIGH);
@@ -46,8 +48,8 @@ void forward (int pwmEnA, int pwmEnB) {
 
  void backward (int pwmEnA, int pwmEnB) {
 
-  analogWrite(EnB, pwmEnA);
-  analogWrite(EnA, pwmEnB);
+  analogWrite(EnB, pwmEnB);
+  analogWrite(EnA, pwmEnA);
   
   digitalWrite(ln3, HIGH);
   digitalWrite(ln4, LOW);
@@ -56,3 +58,38 @@ void forward (int pwmEnA, int pwmEnB) {
   digitalWrite(ln2, HIGH);
 
  }
+
+ void turnRight(int pwmEnA, int pwmEnB){
+  analogWrite(EnB, pwmEnB);
+  analogWrite(EnA, pwmEnA);
+  
+  digitalWrite(ln3, LOW);
+  digitalWrite(ln4, HIGH);
+
+  digitalWrite(ln1, LOW);
+  digitalWrite(ln2, HIGH);
+
+ }
+
+ void turnLeft(int pwmEnA, int pwmEnB){
+  analogWrite(EnB, pwmEnB);
+  analogWrite(EnA, pwmEnA);
+  
+  digitalWrite(ln3, HIGH);
+  digitalWrite(ln4, LOW);
+
+  digitalWrite(ln1, HIGH);
+  digitalWrite(ln2, LOW);
+ }
+
+ /*
+quay 90
+  turnRight(120,100);
+  delay(500);
+  turnRight(0,0);
+  delay(300);
+  turnLeft(100,120);
+  delay(470);
+  turnLeft(0,0);
+  delay(300);
+ */
